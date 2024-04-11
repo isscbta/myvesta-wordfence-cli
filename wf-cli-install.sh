12f2420b8e3faa831f319c1ea6e36e5f7d35a347
myvesta <38690722+myvesta@users.noreply.github.com>
2024-04-11 15:29:21 +0200

    Update wf-cli-install.sh - adding echoes

--- wf-cli-install.sh
+++ wf-cli-install.sh
@@ -17,47 +17,52 @@ echo '   - WordFence CLI'
 
 # Function to install Docker
 install_docker() {
-    echo "Checking for Docker..."
+    echo "= Checking for Docker..."
     if ! command -v docker &> /dev/null; then
-        echo "Docker not found. Starting Docker installation."
-        apt update
-        apt-get install -y ca-certificates curl
+        echo "= Docker not found. Starting Docker installation."
+        apt update > /dev/null 2>&1
+        apt-get install -y ca-certificates curl > /dev/null 2>&1
+        echo "= Installing docker repo"
         install -m 0755 -d /etc/apt/keyrings && \
         curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc && \
         chmod a+r /etc/apt/keyrings/docker.asc && \
-        echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null && \
+        echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+        echo "= Installing docker"
         apt update && apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
         if ! command -v docker &> /dev/null; then
-            echo "Error installing docker !!!";
+            echo "- Error installing docker !!!";
             exit 1;
         fi
+        echo "= Creating docker group"
         newgrp docker
+        echo "= Installing docker-compose"
         curl -s https://api.github.com/repos/docker/compose/releases/latest | grep browser_download_url | grep docker-compose-linux-x86_64 | cut -d '"' -f 4 | wget -qi - && \
         chmod +x docker-compose-linux-x86_64 && \
         mv docker-compose-linux-x86_64 /usr/local/bin/docker-compose
         # docker-compose version
         systemctl restart docker
-        echo "Docker installation completed."
+        echo "= Docker installation completed."
     else
-        echo "Docker is already installed."
+        echo "= Docker is already installed."
     fi
 }
 
 # Function to check and install Git if needed
 install_git() {
-    echo "Checking for Git..."
+    echo "= Checking for Git..."
     if ! command -v git &> /dev/null; then
-        echo "Git not found. Starting Git installation."
-        apt update && apt install -y git
-        echo "Git installation completed."
+        echo "= Git not found. Starting Git installation."
+        apt update > /dev/null 2>&1
+        apt install -y git
+        echo "= Git installation completed."
     else
-        echo "Git is already installed."
+        echo "= Git is already installed."
     fi
 }
 
 # Install and configure WordFence CLI
 install_wordfence_cli() {
-    echo "Starting WordFence CLI installation..."
+    echo "= Starting WordFence CLI installation..."
     if [ -d "/root/wordfence-cli" ]; then
         cd /root/wordfence-cli
         git pull
@@ -66,10 +71,10 @@ install_wordfence_cli() {
         cd ~/wordfence-cli
     fi
     docker build -t wordfence-cli:latest .
-    echo "WordFence CLI installation completed."
+    echo "= WordFence CLI installation completed."
 
     if [ ! -d "/root/wfcli-conf" ]; then
-        echo "Starting WordFence CLI configuration..."
+        echo "= Starting WordFence CLI configuration..."
         docker run -it -v /var/www:/var/www wordfence-cli:latest configure
         CONFCONTAINER=$(docker ps -a | grep 'wordfence configure' | head -n 1 | awk '{print $NF}')
         docker start "$CONFCONTAINER"
@@ -79,10 +84,10 @@ install_wordfence_cli() {
         mkdir -p ~/wfcli-conf
         echo "$CONFCONTENT" > ~/wfcli-conf/wordfence-cli.ini
         if [ -s ~/wfcli-conf/wordfence-cli.ini ]; then
-            echo "WordFence CLI configuration completed successfully."
+            echo "= WordFence CLI configuration completed successfully."
             cat ~/wfcli-conf/wordfence-cli.ini
         else
-            echo "WordFence CLI configuration failed. The configuration file is empty or missing."
+            echo "- WordFence CLI configuration failed. The configuration file is empty or missing."
         fi
     fi
 }
@@ -98,6 +103,7 @@ final_setup() {
     cp /root/vesta/bin/v-wf-malware-scan /usr/local/vesta/bin/v-wf-malware-scan
     cp /root/vesta/bin/v-wf-vulnerability-scan /usr/local/vesta/bin/v-wf-vulnerability-scan
     cp /root/vesta/bin/v-wf-remediate /usr/local/vesta/bin/v-wf-remediate
+    echo "==============================="
     echo "WordFence CLI is ready to use."
     echo "==============================="
     echo ""
